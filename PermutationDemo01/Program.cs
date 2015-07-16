701e0246fc1281e1aee5bf35b2da9e5c57a5d336
ChaunceyAnderson <cta1218@yahoo.com>
2015-07-16 12:41:10 -0400

    Added first version of logic

--- PermutationDemo01/Program.cs
+++ PermutationDemo01/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PermutationDemo01 {
-	class Program {
-		static void Main(string[] args) {
+namespace PermutationExample {
+	public class Program {
+		private static void Swap(ref char a, ref char b) {
+			if (a == b) return;
+
+			a ^= b;
+			b ^= a;
+			a ^= b;
+		}
+
+		public static void GetPer(char[] list) {
+			int x = list.Length - 1;
+			GetPer(list, 0, x);
+		}
+
+		private static void GetPer(char[] list, int k, int m) {
+			if (k == m) {
+				//Console.Write(list);
+				Console.WriteLine(list);
+
+			} else
+				for (int i = k; i <= m; i++) {
+					Swap(ref list[k], ref list[i]);
+					GetPer(list, k + 1, m);
+					Swap(ref list[k], ref list[i]);
+				}
+		}
+
+		static void Main() {
+			string str = "aargen";
+			char[] arr = str.ToCharArray();
+			GetPer(arr);
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("Main method complete. Press Enter");
+			Console.ReadLine();
+
 		}
 	}
 }
